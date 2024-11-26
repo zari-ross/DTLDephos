df34e2f33f036ee0b87e2554ceaac2c2fe74a433
zari-ross <trushina.nataliya.bio@gmail.com>
2024-11-26 23:03:38 +0100

    Not working: Add gene conversion functionality for human-to-rat homologs

--- Phosphatase_translation_to_rat.R
+++ Phosphatase_translation_to_rat.R
@@ -5,6 +5,46 @@ library(biomaRt)
 # Load DEPOD interaction data
 depod_data <- readxl::read_excel("PPase_protSubtrates_201903.xls")
 
+# # Connect to Ensembl database
+# human_mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+# rat_mart <- useMart("ensembl", dataset = "rnorvegicus_gene_ensembl")
+# 
+# # Get human-to-rat homologs
+# human_genes <- c("TP53", "BRCA1", "EGFR") # Replace with your HGNC symbols
+# homologs <- getLDS(
+#   attributes = c("hgnc_symbol"),
+#   filters = "hgnc_symbol",
+#   values = human_genes,
+#   mart = human_mart,
+#   attributesL = c("external_gene_name"),
+#   martL = rat_mart
+# )
+# 
+# # View results
+# print(homologs)
+
+# ---------------------------------------------------------------------------- #
+
+# library(gprofiler2)
+# 
+# human_genes <- c("TP53", "BRCA1", "EGFR") # Replace with your genes
+# 
+# result <- gconvert(
+#   query = human_genes,
+#   organism = "hsapiens", # Source organism
+#   target = "rnorvegicus" # Target organism
+# )
+# 
+# validated_genes <- gconvert(
+#   query = human_genes,
+#   organism = "hsapiens",
+#   target = NULL
+# )
+# 
+# print(validated_genes)
+
+# ---------------------------------------------------------------------------- #
+
 # Map protein accessions to gene names using biomaRt
 mart <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 
